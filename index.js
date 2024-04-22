cc6b571ce3552b99bee4ced909e05f04f27ca6d2
ChoudhryAwais <38686813+ChoudhryAwais@users.noreply.github.com>
2024-04-23 01:21:51 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,30 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+const todoList = [];
+let loop = true;
+while (loop) {
+    let options = await inquirer.prompt([
+        {
+            name: "todoOptions",
+            message: "Select from below:-",
+            type: "list",
+            choices: ["Add new item", "Todo List"]
+        },
+    ]);
+    if (options.todoOptions === "Add new item") {
+        let todoItem = await inquirer.prompt([
+            {
+                name: "item",
+                message: "Enter the todo item",
+                type: "string",
+            },
+        ]);
+        todoList.push(todoItem.item);
+    }
+    else {
+        todoList.forEach(element => {
+            console.log(element);
+        });
+        loop = false;
+    }
+}
